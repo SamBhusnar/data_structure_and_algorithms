e7e5e7e09f9071f2bd9686f5e4dc28b9c9f9c8e0
samadhan <samadhanbhusnar21@gmail.com>
2026-03-08 16:51:12 +0530

    pom.xml was changed!

---
+++ src/main/java/org/example/InfixToPrefix.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class InfixToPrefix {
+    private int top;
+    private int size;
+    private   int []items;
+    InfixToPrefix(int size){
+        top=-1;
+        this.size=size;
+        this.items=new int[size];
+
+    }
+
+
+}
